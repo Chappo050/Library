43f2eea4552e63c47df36667ca00b895d2447c0e
Matthew <matthewchaplin60@gmail.com>
2021-08-19 18:32:57 +0900

    Added input fields

--- script.js
+++ script.js
@@ -2,8 +2,13 @@ let myLibrary = [];
 const addBookBtn = document.getElementById("newBookBtn");
 const refreshTableBtn = document.getElementById("refreshTableBtn");
 const body = document.getElementsByTagName("body")[0];
+const tableFields = 5;
 
-function Book(title, author, pages, read, index){
+//elements
+const radRead = document.getElementById("bookReadTrue");
+const radNotRead = document.getElementById("bookReadFalse");
+
+function Book(title, author, pages, read){
     this.title = title;
     this.author = author;
     this.pages = pages;
@@ -39,16 +44,31 @@ function showLibrary(library) {
     const table = document.createElement("table");
     const tableBody = document.createElement("tableBody");
     table.id = "libraryTable";
+    let cellText = undefined;
+
+    //Create Header Row
+    const headerRow = document.createElement("tr");
+    
+    for (let i = 0; i < tableFields; i++) {
+        const headerCell = document.createElement('td')
+        const fieldArr = ["Title", "Author", "Pages", "Read", "Delete"]
+        cellText = document.createTextNode(fieldArr[i]);
+        headerCell.appendChild(cellText);
+        headerRow.appendChild(headerCell);
+        headerRow.style.backgroundColor = "lightgrey"
+    }
+    tableBody.appendChild(headerRow);
 
     //create all cells
     library.forEach(book => {
         const currentBook = book.title;
         const row = document.createElement('tr');
 
-        for (let i = 0; i < 5; i++) {
+
+
+        for (let i = 0; i < tableFields; i++) {
             
         const cell = document.createElement("td");
-        let cellText = undefined;
 
         switch (i) {
             case 0:
@@ -97,21 +117,37 @@ function showLibrary(library) {
 }
 
 function addBookFunction() {
-    const bookName = prompt("Enter the name of the book.");
-    const bookAuthor = prompt("Enter the Author of the book.");
-    const bookPages = prompt("Enter the amount of pages.");
-    const bookRead = prompt("Has the book been read? (Read/Not Read)");
-    const bookIndex = myLibrary.length +1;
-   const newBook = new Book(bookName, bookAuthor, bookPages, bookRead, bookIndex);
-   addBookToLibrary(newBook);
-   console.log(`${newBook.info()} has been added to the library`);
-   showLibrary(myLibrary);
+    const bookName = document.getElementById("bookTitle");
+    const bookAuthor = document.getElementById("bookAuthor");
+    const bookPages = document.getElementById("bookPages");
+
+    let bookRead = false;
+
+    if (radRead.checked) {
+        bookRead = true;
+    }
+    else{
+        bookRead = false;
+    }
+
+    const newBook = new Book(bookName.value, bookAuthor.value, bookPages.value, bookRead);
+    addBookToLibrary(newBook);
+    console.log(`${newBook.info()} has been added to the library`);
+
+    //Clear all fields
+    bookName.value = null;
+    bookAuthor.value = null;
+    bookPages.value = null;
+    radRead.checked = false;
+    radNotRead.checked =false;
+    showLibrary(myLibrary);
 }
 
 addBookBtn.addEventListener("click", addBookFunction);
 addBookBtn.addEventListener("click", addBookFunction);
 refreshTableBtn.addEventListener("click", function () {showLibrary(myLibrary);} )
-
+radRead.addEventListener('click', () => radNotRead.checked = false);
+radNotRead.addEventListener('click', () => radRead.checked = false);
 
 
 
